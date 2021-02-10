e0abce832f697596cda6966418e5527e840fd7a2
LimMingYeu <jasonlimmingyeu429@gmail.com>
2021-02-10 09:37:36 +0800

    Fragment

---
+++ app/src/main/java/com/example/myapplication/FragmentProfile.kt
@@ -0,0 +1,42 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FragmentProfile.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FragmentProfile : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val v = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        val btn = v.findViewById<View>(R.id.button2)
+        btn.setOnClickListener() {
+            val profile = WelcomeFragment()
+
+            activity!!.supportFragmentManager.beginTransaction().apply {
+                replace(R.id.myFrame, profile)
+
+                addToBackStack(null)
+
+                commit()
+            }
+        }
+        return v
+    }
+}
